5bb52a127f629e132cce964eb0ee41abda1b8710
Dr. Ernesto Lee <socrates73@gmail.com>
2024-12-04 20:41:25 -0500

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -5,8 +5,21 @@ import pickle
 # Load the trained model and features
 with open("churn_model.pkl", "rb") as file:
     data = pickle.load(file)
+
+# Check if the loaded object is a dictionary
+if isinstance(data, dict):
     model = data['model']
     model_features = data['features']
+else:
+    model = data
+    model_features = [
+        'accountlength', 'numbervmailmessages', 'totaldayminutes', 
+        'totaldaycalls', 'totaldaycharge', 'totaleveminutes', 
+        'totalevecalls', 'totalevecharge', 'totalnightminutes', 
+        'totalnightcalls', 'totalnightcharge', 'totalintlminutes', 
+        'totalintlcalls', 'totalintlcharge', 'numbercustomerservicecalls', 
+        'internationalplan_yes', 'voicemailplan_yes'
+    ]
 
 # Define a function for predictions
 def predict_churn(input_data):
@@ -32,8 +45,6 @@ st.write("Input customer data below to predict if they will churn.")
 # Collect user input with default values
 input_data = {
     "accountlength": st.number_input("Account Length", min_value=0, value=120),
-    "internationalplan_yes": st.selectbox("International Plan", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No"),
-    "voicemailplan_yes": st.selectbox("Voicemail Plan", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No"),
     "numbervmailmessages": st.number_input("Number of Voicemail Messages", min_value=0, value=10),
     "totaldayminutes": st.number_input("Total Day Minutes", min_value=0.0, value=250.0),
     "totaldaycalls": st.number_input("Total Day Calls", min_value=0, value=110),
@@ -47,7 +58,9 @@ input_data = {
     "totalintlminutes": st.number_input("Total International Minutes", min_value=0.0, value=15.0),
     "totalintlcalls": st.number_input("Total International Calls", min_value=0, value=4),
     "totalintlcharge": st.number_input("Total International Charge", min_value=0.0, value=3.5),
-    "numbercustomerservicecalls": st.number_input("Number of Customer Service Calls", min_value=0, value=2)
+    "numbercustomerservicecalls": st.number_input("Number of Customer Service Calls", min_value=0, value=2),
+    "internationalplan_yes": st.selectbox("International Plan", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No"),
+    "voicemailplan_yes": st.selectbox("Voicemail Plan", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
 }
 
 # Predict button
